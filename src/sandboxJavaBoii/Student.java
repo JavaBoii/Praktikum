a884806c0c3f3852a8c4a7ba222ea118c54a1ebd
JavaBoii <79570962+JavaBoii@users.noreply.github.com>
2023-05-27 22:14:39 +0200

    GUI Testing V1 Sandbox

---
+++ src/sandboxJavaBoii/Student.java
@@ -0,0 +1,55 @@
+package sandboxJavaBoii;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Student(String name) {
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+        book.setBorrowed(true);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+        book.setBorrowed(false);
+    }
+
+    public void displayBorrowedBooks() {
+        for (Book book : borrowedBooks) {
+            System.out.println(book.toString(true));
+        }
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public boolean isBorrowed(Book book) {
+        return borrowedBooks.contains(book);
+    }
+
+    public boolean isBorrowed(String title) {
+        for (Book book : borrowedBooks) {
+            if (book.getTitle().equals(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+
+
