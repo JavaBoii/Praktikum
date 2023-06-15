16de236900cec79c542d045581fcb337f7416de9
JavaBoii <79570962+JavaBoii@users.noreply.github.com>
2023-06-15 15:18:56 +0200

    Update Point.java

--- src/zettel_6/Point.java
+++ src/zettel_6/Point.java
@@ -13,6 +13,7 @@ public class Point<T extends Number> {
         return Math.sqrt(Math.pow(this.x.doubleValue() - other.x.doubleValue(), 2)
                 + Math.pow(this.y.doubleValue() - other.y.doubleValue(), 2)
                 + Math.pow(this.z.doubleValue() - other.z.doubleValue(), 2));
+        //D = sqrt( (x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2 )
     }
 
     @Override
